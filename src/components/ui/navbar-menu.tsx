c6f81eeed157bb946d1492994f0fc2cdbdf3d4f5
cheongyeechian <cheongyeechian@gmail.com>
2025-03-13 22:25:15 +0800

    navbar styling

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -47,7 +47,7 @@ export const MenuItem = ({
               >
                 <motion.div
                   layout // layout ensures smooth animation
-                  className="w-max h-full p-4"
+                  className="w-max h-full text-nowrap"
                 >
                   {children}
                 </motion.div>
@@ -72,42 +72,10 @@ export const Menu = ({
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
-      className="relative rounded-full border border-yellow-300 bg-[#09092f] shadow-input flex justify-center space-x-4 px-8 py-6 "
+      className="relative border border-yellow-300 bg-[#09092f] shadow-input flex justify-center gap-x-4 text-nowrap py-3 px-2 md:px-4 max-w-full rounded-full"
     >
       {children}
     </nav>
-  );
-};
-
-export const ProductItem = ({
-  title,
-  description,
-  href,
-  src,
-}: {
-  title: string;
-  description: string;
-  href: string;
-  src: string;
-}) => {
-  return (
-    <Link href={href} className="flex space-x-2">
-      <Image
-        src={src}
-        width={140}
-        height={70}
-        alt={title}
-        className="flex-shrink-0 rounded-md shadow-2xl"
-      />
-      <div>
-        <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
-          {title}
-        </h4>
-        <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
-          {description}
-        </p>
-      </div>
-    </Link>
   );
 };
 
